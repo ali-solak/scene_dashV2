01795fb4b4ae211af277f02c8dfd366da39bbe03
Ali Solak <solali@gmx.at>
2026-07-26 15:01:36 +0200

    deploy combat_sample web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"7bef8f54e62e79db15acc2eddab6c6ef8bc9d8
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3663564671" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3226508558" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
